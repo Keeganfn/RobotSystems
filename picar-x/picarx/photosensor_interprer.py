19170ec435b7085fe434bc6468e0cc3aa3010efe
keeganfn <keegan.nave@gmail.com>
2023-01-26 13:42:46 -0800

    Moving average

--- picar-x/picarx/photosensor_interprer.py
+++ picar-x/picarx/photosensor_interprer.py
@@ -1,7 +1,7 @@
 import time
 import os
 import atexit
-from collections import
+from collections import deque
 
 
 class PhotosensorInterpreter():
@@ -10,25 +10,33 @@ class PhotosensorInterpreter():
         self.l_thresh = light_thresh
         self.d_thresh = dark_thresh
         self.polarity = polarity
-        self.g1 = None
-        self.g2 = None
-        self.g3 = None
-        data = []
+        self.g1 = deque([], maxlen=10)
+        self.g2 = deque([], maxlen=10)
+        self.g3 = deque([], maxlen=10)
+        data = deque([], maxlen=10)
 
 
     def check_center(self, grayscale):
-        if self.g1 is None:
-            self.g1 = grayscale[0]
-        if self.g2 is None:
-            self.g2 = grayscale[1]
-        if self.g3 is None:
-            self.g3 = grayscale[2]
+        # if self.g1 is None:
+        #     self.g1 = grayscale[0]
+        # if self.g2 is None:
+        #     self.g2 = grayscale[1]
+        # if self.g3 is None:
+        #     self.g3 = grayscale[2]
+        if len(self.g1) == 0:
+            self.g1.append(grayscale[0])
+            self.g2.append(grayscale[1])
+            self.g3.append(grayscale[2])
 
-        curr_g1 = self.g1 - grayscale[0] 
-        curr_g2 = self.g2 - grayscale[1]         
-        curr_g3 = self.g3 - grayscale[2] 
+        curr_g1 = sum(self.g1)/len(self.g1) - grayscale[0] 
+        curr_g2 = sum(self.g2)/len(self.g2) - grayscale[0] 
+        curr_g3 = sum(self.g3)/len(self.g3) - grayscale[0] 
+
         print("CURRENT", [curr_g1, curr_g2, curr_g3])
 
+        self.g1.append(grayscale[0])
+        self.g2.append(grayscale[1])
+        self.g3.append(grayscale[2])
         left = grayscale[1] - grayscale[0]
         right = grayscale[1] - grayscale[2]
 
